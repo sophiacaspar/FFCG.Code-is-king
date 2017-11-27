0c561d3c5c30d5cd37ce43156e8189ce4b818919
sophiacaspar <sophiacaspar@hotmail.com>
2017-11-27 16:32:08 +0100

    refactored code

--- FFCG.Generation.Meteorolog/FFCG.Generation.Meteorolog/Meteorolog.cs
+++ FFCG.Generation.Meteorolog/FFCG.Generation.Meteorolog/Meteorolog.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Linq;
-using System.IO;
 using System.Collections.Generic;
-using System.Globalization;
 
 namespace FFCG.Generation.Meteorolog
 {
@@ -38,42 +36,7 @@ namespace FFCG.Generation.Meteorolog
         }
     }
 
-    public class FileReader
-    {
-        private UnixTimestampConverter _unixTimestampConverter = new UnixTimestampConverter();
-        private string _pathToFile;
-        private List<DailyValues> _dailyValues = new List<DailyValues>();
-        private Meteorolog _meteorolog = new Meteorolog();
 
-        public FileReader(string pathToFile)
-        {
-            _pathToFile = pathToFile;
-        }
-
-        public void ReadFile()
-        {
-            var csvLines = File.ReadAllLines(_pathToFile).Skip(1);
-            foreach (var csvLine in csvLines)
-            {
-                string[] values = csvLine.Split(';');
-                var unixTimestamp = Convert.ToInt32(values[0].Trim('"'));
-
-                DailyValues dailyvalues = new DailyValues
-                {
-                    Date = _unixTimestampConverter.GetDateFromTimestamp(unixTimestamp),
-                    Time = _unixTimestampConverter.GetTimeFromTimestamp(unixTimestamp),
-                    Temperature = float.Parse(values[1].Trim('"'), CultureInfo.InvariantCulture.NumberFormat)
-                };
-                _dailyValues.Add(dailyvalues);
-            }
-                                           
-        }
-
-        public void PresentDailyValues()
-        {
-            _meteorolog.PresentDailyValues(_dailyValues);
-        }
-    }
 
     public class DailyValues
     {
