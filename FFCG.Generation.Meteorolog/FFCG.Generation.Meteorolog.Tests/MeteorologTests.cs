f3cb0683bfcb217c4f9a7d3568de05ab65186bd8
sophiacaspar <sophiacaspar@hotmail.com>
2017-11-28 09:44:42 +0100

    added tests

--- FFCG.Generation.Meteorolog/FFCG.Generation.Meteorolog.Tests/MeteorologTests.cs
+++ FFCG.Generation.Meteorolog/FFCG.Generation.Meteorolog.Tests/MeteorologTests.cs
@@ -47,8 +47,8 @@ namespace FFCG.Generation.Meteorolog.Tests
             DailyValues _dailyvalues3 = new DailyValues
             {
                 Date = "2017-11-28",
-                Time = "12:15:00",
-                Temperature = float.Parse("7.1", CultureInfo.InvariantCulture.NumberFormat)
+                Time = "06:15:00",
+                Temperature = float.Parse("-3.7", CultureInfo.InvariantCulture.NumberFormat)
             };
             _dailyValues.Add(_dailyvalues3);
 
@@ -72,6 +72,30 @@ namespace FFCG.Generation.Meteorolog.Tests
             string timeCorrespondingToUnixTimestamp = "10:00:00";
             string date = _unixTimestampConverter.GetTimeFromTimestamp(unixTimestamp);
             Assert.AreEqual(timeCorrespondingToUnixTimestamp, date);
+        }
+
+        [TestMethod]
+        public void Should_get_the_first_occurrence_below_zero()
+        {
+            DailyValues firstBelowZero = _meteorolog.GetFirstOccurrenceOfBelowZero(_dailyValues);
+            float temperature = float.Parse("-0.1", CultureInfo.InvariantCulture.NumberFormat);
+            Assert.AreEqual(temperature, firstBelowZero.Temperature);
+        }
+
+        [TestMethod]
+        public void Should_get_the_coldest_temperature()
+        {
+            DailyValues coldest = _meteorolog.GetColdestOccurrence(_dailyValues);
+            float temperature = float.Parse("-3.7", CultureInfo.InvariantCulture.NumberFormat);
+            Assert.AreEqual(temperature, coldest.Temperature);
+        }
+
+        [TestMethod]
+        public void Should_get_the_warmest_temperature()
+        {
+            DailyValues warmest = _meteorolog.GetWarmestOccurrence(_dailyValues);
+            float temperature = float.Parse("3.2", CultureInfo.InvariantCulture.NumberFormat);
+            Assert.AreEqual(temperature, warmest.Temperature);
         }
     }
 }
